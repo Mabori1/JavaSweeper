50512e183f8a3ae81a70d42916e070a347523473
Alexey Makeev <makeevhome@mail.ru>
2022-08-03 12:45:24 +0300

    commit 1

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,21 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class JavaSweeper {
+public class JavaSweeper extends JFrame {
 
     public static void main(String[] args) {
         new JavaSweeper();
     }
     private JavaSweeper(){
-        initJPanel();
-        initJframe();
+        initPanel();
+        initframe();
     }
 
-    private void initJPanel() {
+    private void initPanel() {
         JPanel panel = new JPanel();
+       panel.setPreferredSize(new Dimension(500,500));
+       add(panel);
 
     }
 
-    private void initJframe() {
-
+    private void initframe() {
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
