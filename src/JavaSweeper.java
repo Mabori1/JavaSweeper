8d7db36db8744a25708f5acc1322f563a8b965c5
Alexey Makeev <makeevhome@mail.ru>
2022-08-03 13:16:29 +0300

    commit 2 added icon

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,21 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class JavaSweeper extends JFrame {
+
+    private final int COWS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
 
     public static void main(String[] args) {
         new JavaSweeper();
     }
-    private JavaSweeper(){
+
+    private JavaSweeper() {
         initPanel();
         initframe();
     }
 
     private void initPanel() {
-        JPanel panel = new JPanel();
-       panel.setPreferredSize(new Dimension(500,500));
-       add(panel);
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(getImage("bomb"), 0 ,0, this);
+            }
+        };
 
+        panel.setPreferredSize(new Dimension(COWS*IMAGE_SIZE, ROWS* IMAGE_SIZE));
+        add(panel);
     }
 
     private void initframe() {
@@ -25,5 +38,11 @@ public class JavaSweeper extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private Image getImage(String name){
+        String filename = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource(filename)));
+        return icon.getImage();
     }
 }
