376b580d65ddd7b603893fd1e076deea44d3aad5
Alexey Makeev <makeevhome@mail.ru>
2022-08-03 12:28:00 +0300

    Initial commit

---
+++ src/JavaSweeper.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class JavaSweeper {
+
+    public static void main(String[] args) {
+        new JavaSweeper();
+    }
+    private JavaSweeper(){
+        initJPanel();
+        initJframe();
+    }
+
+    private void initJPanel() {
+        JPanel panel = new JPanel();
+
+    }
+
+    private void initJframe() {
+
+    }
+}
